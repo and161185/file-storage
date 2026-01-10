207a0999b8ae9692d1dfd0caec04bbd8bd5e8aea
an-denisov <a.denisov.toimart@gmail.com>
2026-01-10 12:59:15 +0000

    errortest

--- internal/files/service_test.go
+++ internal/files/service_test.go
@@ -1,7 +1,18 @@
-package files
+package files_test
 
-import "testing"
+import (
+	"context"
+	"file-storage/internal/storage/inmemory"
+	"testing"
+)
 
 func TestUpdate(t *testing.T) {
 
+	ctx := context.Background()
+	//	cfg := config.Image{Ext: "jpeg", MaxDimention: 2000}
+	strg := inmemory.New()
+	f, err := strg.Info(ctx, "1")
+	//s := files.NewService(&cfg, strg)
+
+	//s.Update()
 }
